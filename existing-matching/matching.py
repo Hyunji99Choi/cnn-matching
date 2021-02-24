3caa717f599456c6b9eaf63729b887161b3b2743
Hyunji99Choi <tyg06229@gmail.com>
2021-02-25 03:21:24 +0900

    2021.02.25 flann 파라미터 cnn기반과 맞추기

--- existing-matching/matching.py
+++ existing-matching/matching.py
@@ -19,6 +19,18 @@ sift = cv2.xfeatures2d.SIFT_create()
 kp1, des1 = sift.detectAndCompute(img1,None)
 kp2, des2 = sift.detectAndCompute(img2,None)
 
+
+# cnn 기반 매칭 파라미터와 동일
+# Flann特征匹配, Flann 특징 일치(FLANN 기반 매칭)
+FLANN_INDEX_KDTREE = 1
+index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+search_params = dict(checks=40)
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+# 설정한 FLANN으로 매칭
+matches = flann.knnMatch(des1, des2, k=2)
+
+'''
+# 블로그 매칭 파라미터
 # FLANN parameters
 FLANN_INDEX_KDTREE = 0
 
@@ -27,6 +39,8 @@ search_params = dict(checks=50)   # or pass empty dictionary
 
 flann = cv2.FlannBasedMatcher(index_params,search_params)
 matches = flann.knnMatch(des1,des2,k=2)
+'''
+
 
 # Need to draw only good matches, so create a mask
 matchesMask = [[0,0] for i in range(len(matches))]
