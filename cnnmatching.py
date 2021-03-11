f03c641a78081d198b2704b0fa4e06c336e90156
Hyunji99Choi <tyg06229@gmail.com>
2021-03-11 10:25:56 +0900

    2021.03.11 미팅 완료. 다음 목표는 매칭 정확도 측정 방법 조사

--- cnnmatching.py
+++ cnnmatching.py
@@ -62,6 +62,7 @@ min_dist = 1000
 max_dist = 0
 disdif_avg = 0
 # 统计平均距离差 , 통계평균거리차
+# 매칭 점의 평균 거리차
 for m, n in matches:
     disdif_avg += n.distance - m.distance
 disdif_avg = disdif_avg / len(matches)
